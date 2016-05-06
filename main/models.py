af2a18d3ef6188856079459f1de639d46771add4
FranPitri <franpitri@gmail.com>
2016-05-06 11:47:20 -0300

    agrego modelos de blog

--- main/models.py
+++ main/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User)
+    nationality = models.CharField(max_lenght = 32,
+                                  default = "")
+
+class Post(models.Model):
+    title = models.CharField("Titulo",
+                            max_lenght = 32)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    publicated = models.BooleanField(default = True)
+    author = models.ForeignKeyField(Author)
+
+class Comment(models.Model):
+    author = models.ForeignKeyField(User,
+                                 null = True,
+                                 blank = True)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    post = models.ForeignKeyField(Post)
+    approved = models.BooleanField(default = False)
