9f222c5410d41e3807d33c9a1d9a37a0a8aabf3a
FranPitri <franpitri@gmail.com>
2016-05-08 21:16:13 -0300

    add templates, etc.

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.edit import UpdateView
+
+urlpatterns = patterns('',
+                       url(r'^$',
+                           'main.views.home',
+                           name='home')
+                      )
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+# Create your views here.
+
+from main.models import Post
 
 # Create your views here.
+
+def home(request):
+    context = RequestContext(request)
+    posts = Post.objects.all()
+    context['usuario_actual'] = request.user
+    context['posts'] = posts
+    return render_to_response("hello.html", context)
